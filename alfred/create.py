614fad7cb756cdb0ed5f5012d709af0af46d2a30
robert-v <robertvigorito@gmail.com>
2022-04-24 23:11:44 -0400

    GH-#6: Organized pyproject.py, setup.cfg and config files

---
+++ alfred/create.py
@@ -0,0 +1,224 @@
+"""
+Alfred create helper...
+"""
+import yaml
+import os
+import re
+from alfred.core.connect import wgit
+
+
+_cursor = wgit.cursor()
+commit = wgit.commit
+# TODO: Parse in config
+SHOT_NAME_REGEX = re.compile(r"(?i)(?P<job>[a-z]+)_(?P<scene>\w+)_(?P<code>\w+)")
+
+
+# TODO: Create a configs module
+__dirname = os.path.dirname(__file__
+                            )
+with open(os.path.abspath(f"{__dirname}/configs/alfred.yaml"), "r") as open_config:
+    _config = yaml.safe_load(open_config)
+
+with open(os.path.abspath(f"{__dirname}/configs/structure.yaml"), "r") as open_config:
+    _structure = yaml.safe_load(open_config)
+
+
+def _create_from_config(key):
+    """
+    Create the table if it doesnt exist for the wgid database.
+
+    Returns:
+        True if successful, False otherwise
+    """
+    if not isinstance(key, str) and key in _config:
+        raise ValueError("Please ensure variable is a key of `alfred` configs...")
+
+    columns = " ,".join([f"{key} {item}" for key, item in _config.get(key, {}).items()])
+    cmd = f"CREATE TABLE if not exists {key}s (id INTEGER PRIMARY KEY AUTOINCREMENT, {columns});"
+
+    return cmd
+
+
+def _create_entity(entity, **kwargs):
+    """
+    Creates a row under the entity table, if the row exist
+    update the table column from kwargs.
+
+    Args:
+        entity(str):        Table name
+        **kwargs:           Reference alfred configs
+
+    Returns:
+        (str) sqlite insert command
+    """
+    keys, values = [], []
+    code = kwargs.get("code")
+    if code is None:
+        raise ValueError("Cannot proceed without a unique `code` name...")
+
+    for key, item in _config.get(entity, {}).items():
+        value = kwargs.get(key)
+        if value is None:
+            continue
+
+        keys.append(key), values.append(repr(value))
+
+    insert_keys, insert_values = ", ".join(keys), ", ".join(values)
+    script = \
+        f"""
+        INSERT OR REPLACE INTO {entity}s (id, {insert_keys}) 
+        VALUES ((SELECT id FROM {entity}s WHERE name={repr(kwargs.get("name"))}), 
+                {insert_values});
+        """
+
+    return script
+
+
+def job(**kwargs):
+    """
+    Adds a job entity under the jobs table in the `wgid` database.
+
+    Args:
+        kwargs(dict):       Reference alfred configs
+
+    Returns:
+        True if successful, False otherwise
+    """
+    key = "job"
+    cmd = _create_from_config(key)
+    cmd += _create_entity(key, **kwargs)
+    # Submit SQL and commit changes
+    _cursor.executescript(cmd)
+
+    return True
+
+
+def scene(**kwargs):
+    """
+    Adds a scene entity under the scenes table in the `wgid` database.
+
+    Args:
+        kwargs(dict):       Reference alfred configs
+
+    Returns:
+        True if successful
+    """
+    key = "scene"
+    cmd = _create_from_config(key)
+    cmd += _create_entity(key, **kwargs)
+    # Submit SQL and commit changes
+    _cursor.executescript(cmd)
+
+    return True
+
+
+def shot(**kwargs):
+    """
+    Adds a shot entity under the shots table in the `wgid` database.
+
+    Args:
+        kwargs(dict):       Reference alfred configs
+
+    Returns:
+        True if successful
+    """
+    key = "shot"
+    cmd = _create_from_config(key)
+    cmd += _create_entity(key, **kwargs)
+    # Submit SQL and commit changes
+    _cursor.executescript(cmd)
+
+    return True
+
+
+def shots(**kwargs):
+    """
+    From a dictionary that consist of key `shotname` parsed as `SHOW_SCENE_SHOT`
+    update the item values, create a batch script to insert or update shot list.
+
+    Examples:
+        kwargs = {
+            "MMS_001_001":
+                "frame_range": "1001-1258",
+                "description": "This is a demo",
+            "MMS_001_010": "",
+            "MMS_001_020": ""}
+
+    Args:
+        kwargs(dict):       Reference alfred configs
+
+    Returns:
+        True if successful
+    """
+    script = ""
+    for i, (shot_name, data) in enumerate(kwargs.items()):
+        match = SHOT_NAME_REGEX.match(shot_name)
+        # Check if the shot table has been create
+        if i == 0:
+            script = _create_from_config("shot")
+        # Invalid shot name
+        if match is None:
+            continue
+
+        data = data if isinstance(data, dict) else dict()
+        data.update(match.groupdict())
+        data["name"] = shot_name
+        script += _create_entity("shot", **data)
+
+    wgit.executescript(script)
+    return True
+
+
+def _recursive_structure(data, path=""):
+    """
+
+    Args:
+        data(dict):         Data from structure.yaml
+
+    Keyword Args:
+        path(str):          Joined path
+
+    Yields:
+        (generator) String of the full folder path
+    """
+    for key, items in data.items():
+        if not items:
+            yield os.path.join(path, key)
+
+        elif isinstance(items, dict):
+            for p in _recursive_structure(items, os.path.join(path, key)):
+                yield p
+
+        elif hasattr(items, "__iter__"):
+            for item in items:
+                yield os.path.join(path, key, item)
+        else:
+            yield os.path.join(path, key, str(items))
+
+
+def structure(job=None, shot_names=None):
+    """
+    Constructs the new project in the jobs directory by parsing the structure
+    configs and create the folder structure.
+
+    Args:
+        job(str):               Job directory to create the shots
+        shot_names(list):       List of `shot name`
+
+    Returns:
+        True if sucessful, False otherwise
+    """
+    for shot_name in shot_names:
+        context = {"job": job, "shot": shot_name, "scene": "scrap"}
+        match = SHOT_NAME_REGEX.match(shot_name)
+
+        if not any([match, job]):
+            continue
+        elif match:
+            context.update(match.groupdict())
+
+        for path in _recursive_structure(_structure, "/jobs"):
+            path = path.format(**context)
+            os.system(f"mkdir -p {path}")
+
+    return True
---
+++ alfred/showtime.py
@@ -0,0 +1,83 @@
+"""
+Showtime
+
+Startup tool, setting configs environments per show needs
+
+# usage
+# showtime {shot_name}
+# showtime -h --help  # Display tree of current shots
+"""
+import argparse
+from anytree import Node, RenderTree
+import alfred.search
+import os
+
+# Creating the arg parser
+parser = argparse.ArgumentParser()
+parser.add_argument("code", help="Show name or shot code")
+parser.add_argument("-t", "--tree", nargs="?", const=True, help="Display the all shots of the show")
+args = parser.parse_args()
+code = args.code
+
+
+def show_tree(job):
+    """
+    Show tree, displays the shots of the show in a tree view manner.
+    Usage to aid artist when referencing assigned shots.
+
+    Examples:
+        show
+            - scene
+                - shot-names
+    Args:
+        job (str):       Job name
+    """
+    print("# " + "-" * 50)
+    print(f"Showtime shots list for `{job.upper()}`\n")
+    query = alfred.search.scenes_shots(args.code)
+    job_node = Node(job)
+
+    for scene, shots in sorted(query.items()):
+        scene = Node(scene, parent=job_node)
+        for shot in sorted(shots):
+            Node(shot, parent=scene)
+
+    for p, f, node in RenderTree(job_node):
+        print(f"{p}{node.name}")
+
+    print("\nExample:\nshowtime {show} {shot-name}")
+
+
+def its_showtime():
+    """
+    Well its showtime, upon user arguments, validate code or shot name,
+    set environment and show configs.
+    """
+    # Attributes
+    job, scene, shot = "", "", ""
+    # Validate the code if job or shot name
+    shot_data = alfred.search.shots(shot_name=code)
+    if alfred.search.jobs(code):
+        job = code
+
+    elif shot_data:
+        job, scene, shot = shot_data.get("job"), shot_data.get("scene"), code
+
+    else:
+        print("Wrong job code provide, try again with correct code...")
+        exit(1)
+        return False
+
+    # Show the tree of the show inputed
+    if args.tree and not shot_data:
+        show_tree(job)
+        exit(1)
+
+    # Set the environment
+    os.environ["JOB"] = job.upper()
+    os.environ["SCENE"] = scene.upper()
+    os.environ["SHOTNAME"] = shot.upper()
+
+    print("# " + "-" * 50)
+    print(f"Showtime: {shot.upper() or job.upper()}!")
+    return True
